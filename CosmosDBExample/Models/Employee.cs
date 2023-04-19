b49d3baeb6d28601391b6df8595525181021cebb
stescobedo92 <stescobedo.31@gmail.com>
2023-04-19 12:11:28 -0600

    Add Employee, Customer and Order models

---
+++ CosmosDBExample/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosDBExample.Models
+{
+    public class Employee
+    {
+        public string? Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Title { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? HireDate { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
+        public string? HomePhone { get; set; }
+    }
+}
---
+++ CosmosDBExample/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosDBExample.Models
+{
+    public class Order
+    {
+        public string? Id { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public DateTime? RequiredDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public int? ShipVia { get; set; }
+        public double? Freight { get; set; }
+        public string? ShipName { get; set; }
+        public string? ShipAddress { get; set; }
+        public string? ShipCity { get; set; }
+        public string? ShipRegion { get; set; }
+        public string? ShipPostalCode { get; set; }
+        public string? ShipCountry { get; set; }
+    }
+}
